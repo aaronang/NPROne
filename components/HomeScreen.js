ed2692566a73b4271f757811ba05356f20e134c9
Aaron Ang <awz.ang@gmail.com>
2017-10-06 14:14:02 -0700

    Generate and store CSRF token

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -2,7 +2,8 @@ import React from 'react';
 import {
   Linking,
   View,
-  Button
+  Button,
+  AsyncStorage
 } from 'react-native';
 import qs from 'query-string';
 
@@ -12,6 +13,7 @@ export default class HomeScreen extends React.Component {
   };
 
   async componentDidMount() {
+    await this.obtainCsrfToken()
     const url = await Linking.getInitialURL()
     if (url) this.getAccessToken(url)
   }
@@ -20,25 +22,53 @@ export default class HomeScreen extends React.Component {
     const params = qs.parse(url.split('?')[1])
     const code = params.code
     const state = params.state
-    const body = qs.stringify({
-      grant_type: 'authorization_code',
-      client_id: 'nprone_trial_NDez2fJ7qASv',
-      client_secret: '3Qcafw9osRpK8Rf6sgAvhl9xlYFm4269VlkxGUn1',
-      code: code,
-      redirect_uri: 'nprone://authorize'
-    })
 
-    try {
-      const response = await fetch('https://api.npr.org/authorization/v2/token', {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: body
+    if (this.state.csrfToken === state) {
+      const body = qs.stringify({
+        grant_type: 'authorization_code',
+        client_id: 'nprone_trial_NDez2fJ7qASv',
+        client_secret: '3Qcafw9osRpK8Rf6sgAvhl9xlYFm4269VlkxGUn1',
+        code: code,
+        redirect_uri: 'nprone://authorize'
       })
-      const token = await response.json();
-      this.setState({ token: token })
+
+      try {
+        const response = await fetch('https://api.npr.org/authorization/v2/token', {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded'
+          },
+          body: body
+        })
+        const token = await response.json();
+        console.log(token)
+        this.setState({ token: token })
+      } catch (error) {
+        console.error(error)
+      }
+    } else {
+      console.error('State does not match.')
+    }
+  }
+
+  async generateCsrfToken() {
+    const csrfToken = Math.random().toString(36).substring(2)
+    try {
+      await AsyncStorage.setItem('@NPROneStore:CSRF_TOKEN', csrfToken);
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async obtainCsrfToken() {
+    try {
+      const value = await AsyncStorage.getItem('@NPROneStore:CSRF_TOKEN');
+      if (value !== null) {
+        this.setState({ csrfToken: value })
+      } else {
+        await this.generateCsrfToken()
+      }
     } catch (error) {
       console.error(error)
     }
--- components/LoginScreen.js
+++ components/LoginScreen.js
@@ -1,16 +1,28 @@
 import React from 'react'
 import qs from 'query-string'
-import { WebView } from 'react-native';
+import { WebView, Text, View, AsyncStorage } from 'react-native';
 
 export default class LoginScreen extends React.Component {
-  generateState() {
-    return Math.random().toString(36).substring(2)
+  constructor(props) {
+    super(props)
+    this.state = { loading: true }
+  }
+
+  async componentDidMount() {
+    try {
+      const value = await AsyncStorage.getItem('@NPROneStore:CSRF_TOKEN');
+      if (value !== null) {
+        this.setState({ csrfToken: value, loading: false })
+      }
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   generateAuthUri() {
     const params = qs.stringify({
       client_id: 'nprone_trial_NDez2fJ7qASv',
-      state: this.generateState(),
+      state: this.state.csrfToken,
       redirect_uri: 'nprone://authorize',
       response_type: 'code',
       scope: 'listening.readonly'
@@ -19,8 +31,12 @@ export default class LoginScreen extends React.Component {
   }
 
   render() {
-    return (
-      <WebView source={{ uri: this.generateAuthUri() }} />
-    );
+    if (this.state.loading) {
+      return <View><Text>Loading...</Text></View>
+    } else {
+      return (
+        <WebView source={{ uri: this.generateAuthUri() }} />
+      );
+    }
   }
 }
